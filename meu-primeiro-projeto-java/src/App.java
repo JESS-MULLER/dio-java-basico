2db0aa765941e5b994df37a8c64be28e4a5da835
JESS-MULLER <jess_pan1@hotmail.com>
2024-11-02 21:26:34 -0300

    Meu primeiro projeto java

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Boas Vindas turma Dio!");
+        System.out.println("Boas Vindas turm Dio!");
     }
 }
